083498f30856b8770f2077709edec1a7c1004963
AlexiVides <88692360+AlexiVides@users.noreply.github.com>
2023-02-11 21:41:22 -0600

    Update

--- Inmuebles_Guia2/Inmuebles_Guia2/Program.cs
+++ Inmuebles_Guia2/Inmuebles_Guia2/Program.cs
@@ -11,64 +11,118 @@ namespace Inmuebles_Guia2
     {
         static void Main(string[] args)
         {
-            Empleado empleado = new Empleado();
-            Cliente cliente = new Cliente();
-            Inmueble inmueble = new Inmueble();
-            Plazo plazo = new Plazo();
 
+
+            Accion accion = new Accion();
+
+            Plazo plazo = new Plazo();
             Console.WriteLine("Bienvinido al sistema");
             int x = 0;
-            
-            while(x == 0)
+
+            while (x == 0)
             {
                 Console.WriteLine("-------------------Menu---------------------");
-                Console.WriteLine("1- Elaborar comprar");
-                Console.WriteLine("2-Salir");
-                
+                Console.WriteLine("1- Elaborar una Compra");
+                Console.WriteLine("2- Elaborar una Venta");
+                Console.WriteLine("3- Elaborar una Alquilacion");
+                Console.WriteLine("4-Salir");
+
                 int opc = Convert.ToInt32(Console.ReadLine());
                 Console.Clear();
+                Console.WriteLine("Datos de Empleado");
+                Console.WriteLine("Nombre: ");
+                string Nombre = Console.ReadLine();
+                Console.WriteLine("Apellido: ");
+                string Apellido = Console.ReadLine();
+                Console.WriteLine("Edad: ");
+                int Edad = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Datos capturados");
+                Empleado empleado = new Empleado(Nombre, Apellido, Edad);
 
+                Console.WriteLine("--------------------------");
+                Console.WriteLine("Datos de cliente");
+                Console.WriteLine("Nombre: ");
+                Nombre = Console.ReadLine();
+                Console.WriteLine("Apellido: ");
+                Apellido = Console.ReadLine();
+                Console.WriteLine("Direccion: ");
+                string Direccion = Console.ReadLine();
+                Console.WriteLine("Telefono: ");
+                int Telefono = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Datos capturados");
+                Cliente cliente = new Cliente(Nombre, Apellido, Direccion, Telefono);
+
+                Console.WriteLine("--------------------------");
+                Console.WriteLine("Datos de Inmueble");
+                Console.WriteLine("Codigo: ");
+                string Codigo = Console.ReadLine();
+                Console.WriteLine("Descripcion: ");
+                string Descripcion = Console.ReadLine();
+                Console.WriteLine("Tipo: ");
+                string Tipo = Console.ReadLine();
+                Inmueble inmueble = new Inmueble(Codigo, Descripcion, Tipo);
+                Console.WriteLine("--------------------------");
+                plazo.DatosPlazo();
+                Console.WriteLine("--------------------------");
                 switch (opc)
                 {
-                        case 1:
-                        empleado.DatosEmpleado();
-                        Console.WriteLine("--------------------------");
-                        cliente.DatosCliente();
-                        Console.WriteLine("--------------------------");
-                        inmueble.DatosInmueble();
-                        Console.WriteLine("--------------------------");
-                        plazo.DatosPlazo();
-                        Console.WriteLine("--------------------------");
+                    case 1:
+
 
                         Console.Clear();
-                      
                         Console.WriteLine("---------------------Factura del Inmobiliario---------------------");
                         Console.WriteLine("Nombre del Empleado: " + empleado.Nombre + " " + empleado.Apellido);
                         Console.WriteLine("Nombre del Cliente: " + cliente.Nombre + " " + cliente.Apellido);
-                        Console.WriteLine("Direccion: " + cliente.direccion + " Telefono: " + cliente.telefono);
-                        Console.WriteLine("Codigo: " + inmueble.codigo + " Descripcion: " + inmueble.descripcion);
-                        Console.WriteLine(" Tipo: " + inmueble.tipo + " Plazo: " + plazo.meses);
+                        Console.WriteLine("Direccion: " + cliente.Direccion + " Telefono: " + cliente.Telefono);
+                        accion.Compra();
+                        Console.WriteLine("Codigo: " + inmueble.Codigo + " Descripcion: " + inmueble.Descripcion);
+                        Console.WriteLine(" Tipo: " + inmueble.tipoInmuebele + " Plazo: " + plazo.meses);
                         break;
 
-                        case 2:
-                             Console.WriteLine("Desea Salir?");
-                             x = 1;
+                    case 2:
+                        Console.Clear();
+
+                        Console.WriteLine("---------------------Factura del Inmobiliario---------------------");
+                        Console.WriteLine("Nombre del Empleado: " + empleado.Nombre + " " + empleado.Apellido);
+                        Console.WriteLine("Nombre del Cliente: " + cliente.Nombre + " " + cliente.Apellido);
+                        Console.WriteLine("Direccion: " + cliente.Direccion + " Telefono: " + cliente.Telefono);
+                        accion.Vender();
+                        Console.WriteLine("Codigo: " + inmueble.Codigo + " Descripcion: " + inmueble.Descripcion);
+                        Console.WriteLine(" Tipo: " + inmueble.tipoInmuebele + " Plazo: " + plazo.meses);
                         break;
-                }              
+
+
+                    case 3:
+                        Console.Clear();
+
+                        Console.WriteLine("---------------------Factura del Inmobiliario---------------------");
+                        Console.WriteLine("Nombre del Empleado: " + empleado.Nombre + " " + empleado.Apellido);
+                        Console.WriteLine("Nombre del Cliente: " + cliente.Nombre + " " + cliente.Apellido);
+                        Console.WriteLine("Direccion: " + cliente.Direccion + " Telefono: " + cliente.Telefono);
+                        accion.Alquiler();
+                        Console.WriteLine("Codigo: " + inmueble.Codigo + " Descripcion: " + inmueble.Descripcion);
+                        Console.WriteLine(" Tipo: " + inmueble.tipoInmuebele + " Plazo: " + plazo.meses);
+                        break;
+
+                    case 4:
+                        Console.WriteLine("Desea Salir?");
+                        x = 1;
+                        break;
+
+                }
             }
-
-          
-            
-
         }
 
-       
+
 
     }
-     abstract class Persona
+
+    abstract class Persona
     {
+        //Atributos
         private string nombre;
         private string apellido;
+
 
         public string Nombre
         {
@@ -81,74 +135,87 @@ namespace Inmuebles_Guia2
             get { return apellido; }
             set { apellido = value; }
         }
+
+
     }
 
     class Empleado : Persona
     {
+        //Atributos
+        public int Edad;
 
-        public int edad;
-       
-
-        public void DatosEmpleado()
+        //Constructor 
+        public Empleado(string nombre, string apellido, int edad)
         {
-            Console.WriteLine("Datos de Empleado");
-            Console.WriteLine("Nombre: ");
-            Nombre= Console.ReadLine();
-            Console.WriteLine("Apellido: ");
-            Apellido= Console.ReadLine();
-            Console.WriteLine("Edad: ");
-            edad = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Datos capturados");
-
+            Nombre = nombre;
+            Apellido = apellido;
+            Edad = edad;
         }
+
     }
 
     class Cliente : Persona
-    {    
-        public int telefono;     
-        public string direccion;
-        public void DatosCliente()
+    {
+        //Atributos     
+        public string Direccion;
+        public int Telefono;
+
+        //Constructor
+        public Cliente(string nombre, string apellido, string direccion, int telefono)
         {
-            Console.WriteLine("Datos de cliente");
-            Console.WriteLine("Nombre: ");
-            Nombre = Console.ReadLine();
-            Console.WriteLine("Apellido: ");
-            Apellido = Console.ReadLine();
-            Console.WriteLine("Direccion: ");
-            direccion = Console.ReadLine();
-            Console.WriteLine("Telefono: ");
-            telefono= Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Datos capturados");
+            Nombre = nombre;
+            Apellido = apellido;
+            Direccion = direccion;
+            Telefono = telefono;
         }
     }
 
     class Tipo
     {
-        public string tipo;
+        //Atributo
+        public string tipoInmuebele;
     }
 
     class Inmueble : Tipo
     {
-        public string codigo;
-        public string descripcion;
+        //Atributo
+        public string Codigo;
+        public string Descripcion;
 
-        public void DatosInmueble()
+
+        //Construtor
+        public Inmueble(string codigo, string descripcion, string tipo)
         {
-            Console.WriteLine("Datos de Inmueble");
-            Console.WriteLine("Codigo: ");
-            codigo= Console.ReadLine();
-            Console.WriteLine("Descripcion: ");
-            descripcion= Console.ReadLine();
-            Console.WriteLine("Tipo: ");
-            tipo= Console.ReadLine();
+            Codigo = codigo;
+            Descripcion = descripcion;
+            tipoInmuebele = tipo;
+
+        }
+    }
+
+    class Accion
+    {
+        //Metodo
+        public void Vender()
+        {
+            Console.WriteLine("Operacion a Realizar: Venta");
+        }
+        public void Compra()
+        {
+            Console.WriteLine("Operacion a Realizar: Compra");
+        }
+        public void Alquiler()
+        {
+            Console.WriteLine("Operacion a Realizar: Alquiler");
         }
     }
 
     class Plazo
     {
-
+        //Atributo
         public int meses;
 
+        //Metodo
         public void DatosPlazo()
         {
             Console.WriteLine("Datos de Plazo");
